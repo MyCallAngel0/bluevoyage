9ac60c4efd0c8fe6437c41cbde5e9d8601172f1f
MyCallAngel0 <mihail.mihalachi@isa.utm.md>
2024-10-03 13:22:05 +0300

    Add login required restriction

--- app/urls.py
+++ app/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.registerPage, name="register2"),
+    path('register/', views.registerPage, name="register"),
     path('login/', views.loginPage, name="login"),
     path('logout/', views.logoutUser, name="logout"),
     path('', views.homePage, name="home"),
 
-    path('test/', views.homePage, name="random"),
+    path('test/', views.randomPage, name="random"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+
+from .decorators import unauthenticated_user
 from .forms import CreateUserForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
 
+@unauthenticated_user
 def registerPage(request):
-    if request.user.is_authenticated:
-        return redirect('home')
     form = CreateUserForm()
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
@@ -18,10 +19,8 @@ def registerPage(request):
     return render(request, 'bluevoyage/register.html', context)
 
 
+@unauthenticated_user
 def loginPage(request):
-    print(request.user.is_authenticated)
-    if request.user.is_authenticated:
-        return redirect('home')
     if request.method == "POST":
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -48,6 +47,6 @@ def homePage(request):
 
 
 @login_required(login_url='/login')
-def testPage(request):
+def randomPage(request):
     context = {}
     return render(request, 'random.html', context)
